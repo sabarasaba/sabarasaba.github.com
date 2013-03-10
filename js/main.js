f1a980352e3d0eb63db4810b0c069e8ac5e57c34
Ignacio Rivas <rivasign@gmail.com>
2013-03-10 19:16:47 -0300

    New portfolio design

--- js/main.js
+++ js/main.js
@@ -1,56 +1,68 @@
-$(document).ready(function(){
+$(function(){
 
-	$('.box-filter a').click(function(e){
-		e.preventDefault();
-		
-		var demo = $(this).text();
-		$('a, .box-filter *').removeClass('current-filter');
-		$(this).addClass('current-filter');
+  // --- Active Nav --- //
 
-		switch (demo)
-		{
-			case "All":
-				$('.box-selector-type-all').attr('checked', true);
-			break;
+  var url = window.location.pathname,
+      urlRegExp = new RegExp(url == '/' ? window.location.origin + '/?$' : url.replace(/\/$/,''));
 
-			case "Projects":
-				$('.box-selector-type-1').attr('checked', true);
-			break;
+  $('#pagenav a').each(function(){
+      if(urlRegExp.test(this.href.replace(/\/$/,''))){
+          $(this).addClass('active');
+      }
+  });
 
-			case "Contributions":
-				$('.box-selector-type-2').attr('checked', true);
-			break;
+  // --- Markdown Footnotes --- //
 
-			case "Gists":
-				$('.box-selector-type-3').attr('checked', true);
-			break;
+  $("a[rel=footnote]").each(function(){
+    var link = $(this);
+    var token = link.attr('href').substr(1);
+    var footnoteContent = $(document.getElementById(token)).html();
 
-			case "Posts":
-				$('.box-selector-type-4').attr('checked', true);
-			break;
-		}
-	});	
+    // There is an issue with the line below that stops the clock-tap-scroll-to-top on iOS
+    $('body').append('<div id="overlay-' + token + '" class="footnoteContent">' + footnoteContent + '</div>');
+
+    link.click(function(){
+      var $currentFootnote = $(document.getElementById('overlay-' + token));
+
+      // If the footnote is already displayed, hide it instead
+      if ($currentFootnote.is(':visible')) {
+        $currentFootnote.slideUp('fast');
+
+      } else {
+        $('.footnoteContent').hide();
+        $currentFootnote.slideDown('fast');
+      }
+
+      return false;
+    });
+  });
+
+  $('.footnoteContent a[rev=footnote]').remove();
+  $('.footnoteContent').prepend('<a href="#" class="closeFootnote">&times;</a>');
+  $('.closeFootnote').click(function(){
+    $(this).closest('.footnoteContent').slideUp('fast');
+    return false;
+  });
+
+  // --- Responsive Menu --- //
+
+  var header = $("#masthead"),
+      menu = $("ul#pagenav"),
+      menuButton = $("<div class='menubutton'><a href='#'><span></span><span></span><span></span></a></div>");
+
+  menuButton.click(showMenu);
+  header.append(menuButton);
+
+  function showMenu (event) {
+    if (menu.is(":visible"))
+        menu.slideUp({complete:function(){$(this).css('display','')}});
+    else
+        menu.slideDown();
+  }
+
+  // Prevents # being appended to URL. Also Prevents jumping to the top of the page because of the # anchor.
+  $(".menubutton").click(function(event) {
+    event.preventDefault();
+  });
 
 });
-
-var $headline = $('#headline').hide();
-
-google.load('webfont','1');
-
-google.setOnLoadCallback(function() {
-	WebFont.load({
-		google		: {
-			families	: ['Montserrat','Concert One']
-		},
-		fontactive	: function(fontFamily, fontDescription) {
-			init();
-		},
-		fontinactive	: function(fontFamily, fontDescription) {
-			init();
-		}
-	});
-});
-
-function init() {
-	$headline.show().arctext({radius: 400});
-};
