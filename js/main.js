30b5b1c651a4ffaa9dc517876a664a58fbc8d2a7
Ignacio Rivas <rivasign@gmail.com>
2013-03-16 12:23:18 -0300

    Refactored css

--- js/main.js
+++ js/main.js
@@ -1,52 +1,8 @@
 $(function(){
 
-  // --- Active Nav --- //
-
-  // var url = window.location.pathname,
-  //     urlRegExp = new RegExp(url == '/' ? window.location.origin + '/?$' : url.replace(/\/$/,''));
-
-  // $('#pagenav a').each(function(){
-  //     if(urlRegExp.test(this.href.replace(/\/$/,''))){
-  //         $(this).addClass('active');
-  //     }
-  // });
-
-  // --- Markdown Footnotes --- //
-
-  $("a[rel=footnote]").each(function(){
-    var link = $(this);
-    var token = link.attr('href').substr(1);
-    var footnoteContent = $(document.getElementById(token)).html();
-
-    // There is an issue with the line below that stops the clock-tap-scroll-to-top on iOS
-    $('body').append('<div id="overlay-' + token + '" class="footnoteContent">' + footnoteContent + '</div>');
-
-    link.click(function(){
-      var $currentFootnote = $(document.getElementById('overlay-' + token));
-
-      // If the footnote is already displayed, hide it instead
-      if ($currentFootnote.is(':visible')) {
-        $currentFootnote.slideUp('fast');
-
-      } else {
-        $('.footnoteContent').hide();
-        $currentFootnote.slideDown('fast');
-      }
-
-      return false;
-    });
-  });
-
-  $('.footnoteContent a[rev=footnote]').remove();
-  $('.footnoteContent').prepend('<a href="#" class="closeFootnote">&times;</a>');
-  $('.closeFootnote').click(function(){
-    $(this).closest('.footnoteContent').slideUp('fast');
-    return false;
-  });
-
   // --- Responsive Menu --- //
 
-  var header = $("#masthead"),
+  var header = $(".sidebar"),
       menu = $("ul#pagenav"),
       menuButton = $("<div class='menubutton'><a href='#'><span></span><span></span><span></span></a></div>");
 
@@ -55,7 +11,7 @@ $(function(){
 
   function showMenu (event) {
     if (menu.is(":visible"))
-        menu.slideUp({complete:function(){$(this).css('display','')}});
+        menu.slideUp({complete:function(){$(this).css('display','');} });
     else
         menu.slideDown();
   }
