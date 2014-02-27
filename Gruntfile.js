a9c9ae1a75a15f2e87ecb70142cd09e9a2f919d0
Ignacio Rivas <rivasign@gmail.com>
2014-02-27 19:38:03 -0300

    Defining needed tasks for building the site

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,117 +1,66 @@
 module.exports = function(grunt) {
   'use strict';
 
+  var PATH_ASSETS_CSS = 'css/';
+  var PATH_DEPLOY_ASSETS = 'public/';
+
   // ==========================================================================
   // Project configuration
   // ==========================================================================
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
-    // clean build directory
-    clean: ['deploy'],
+    clean: [PATH_DEPLOY_ASSETS],
 
-    // tasks to be executed and files
-    // to be watched for changes
-    watch: {
-      files: ['./js/app/**/*.hbs', './js/app/**/*.js', './js/test/SpecRunner.js'],
-      tasks: ['clean', 'jshint:all', 'handlebars', 'requirejs', 'concat', 'cssmin', 'copy']
-    },
-
-    // require js
-    requirejs: {
-      compile: {
-        options: {
-          dir: 'deploy',
-          baseUrl: './js',
-          //configuration file
-          mainConfigFile: 'js/main.js',
-          // optimize javascript files with uglifyjs
-          optimize: 'uglify',
-          // define dependencies
-          modules: [
-            {
-              name: 'main'
-            },
-            {
-              name: 'app/application',
-              exclude: ['main']
-            }
-          ]
-        }
-      }
-    },
-
-    //Precompile hbs files into app/template.js
-    //generates the template registry
-    handlebars: {
-      compile: {
-        options: {
-          namespace: 'JST',
-          amd: true,
-          processName: function(filename) {
-            var pieces = filename.split('/');
-            return pieces[pieces.length - 1];
-          }
+    csslint: {
+      lax: {
+        rules: {
+          'box-sizing': false
         },
-        files: {
-          'js/app/templates.js': 'js/**/*.hbs'
-        }
+        src: [
+          PATH_ASSETS_CSS + '/*.css'
+        ]
       }
-    },
-
-    // js linting options
-    jshint: {
-      all: ['Gruntfile.js', 'js/main.js', 'js/app/**/*.js', '!js/app/templates.js', '!js/app/utils/debug.js'],
-      jshintrc: '.jshintrc'
     },
 
     concat: {
       css: {
-        src: ['css/*.css', 'js/app/**/*.css'],
-        dest: 'deploy/css/<%= pkg.name %>-<%= pkg.version %>.concat.css'
+        src: [PATH_ASSETS_CSS + '/*.css'],
+        dest: PATH_DEPLOY_ASSETS +
+          '/css/<%= pkg.name %>.concat.css'
       }
     },
 
     cssmin: {
       my_target: {
-        src: 'deploy/css/<%= pkg.name %>-<%= pkg.version %>.concat.css',
-        dest: 'deploy/css/<%= pkg.name %>.min-<%= pkg.version %>.css'
+        src: PATH_DEPLOY_ASSETS +
+          '/css/<%= pkg.name %>.concat.css',
+        dest: PATH_DEPLOY_ASSETS +
+          '/css/<%= pkg.name %>.min.css'
       }
     },
 
-    karma: {
-      watch: {
-        configFile: 'karma.conf.js'
-      },
-      ci: {
-        configFile: 'karma.conf.js',
-        autoWatch: false,
-        singleRun: true,
-        browsers: ['PhantomJS']
+    watch: {
+      assets: {
+        files: [
+          PATH_ASSETS_CSS + '/*.css',
+          'index.html'
+        ],
+        tasks: ['csslint:lax', 'concat', 'cssmin'],
+        options: {
+          livereload: true
+        }
       }
     }
-
   });
 
-
   grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-requirejs');
-  grunt.loadNpmTasks('grunt-contrib-handlebars');
   grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-csslint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-css');
-  grunt.loadNpmTasks('grunt-karma');
 
-  // default build task
-  grunt.registerTask('default', 'build');
+  grunt.registerTask('default', 'build:dev');
 
-  // build task
-  grunt.registerTask('build', ['clean', 'jshint:all', 'handlebars', 'requirejs', 'concat', 'cssmin']);
-
-  // test task
-  grunt.registerTask('test', ['karma:watch']);
-
-  // watch task
-  grunt.registerTask('w', ['watch']);
+  grunt.registerTask('build:dev', ['clean', 'csslint:lax', 'concat', 'cssmin', 'watch:assets']);
 };
