4dd350f97068fd02c7fd8c628eb243bc9f5388bb
Ignacio Rivas <rivasign@gmail.com>
2021-05-21 08:57:39 +0200

    upgrade deps

--- src/components/experience/index.js
+++ src/components/experience/index.js
@@ -5,12 +5,13 @@ export default () => (
     <p className="sign">@@@@@@@@@@@@@@@@@@@@@@@@@</p>
     <h2 className="big">Experience.</h2>
     <ul>
-      <li><span className="star">*</span> (Feb 2018-Present) <b>Front-End Engineer</b> @ReaQta, Amsterdam</li>
+      <li><span className="star">*</span> (May 2021-Present)  <b>Software Engineer</b> @Elastic, Earth</li>
+      <li><span className="star">*</span> (Feb 2018-Mar 2021) <b>Front-End Engineer</b> @ReaQta, Amsterdam</li>
       <li><span className="star">*</span> (Jan 2017-Feb 2018) <b>Front-End Engineer</b> @Deskbookers, Amsterdam</li>
-      <li><span className="star">*</span> (Aug 2016) <b>Full-Stack Engineer</b> @mycujoo, Amsterdam</li>
-      <li><span className="star">*</span> (Apr 2015) <b>Front-End Engineer</b> @Osper, London</li>
-      <li><span className="star">*</span> (May 2014) <b>Front-End Engineer</b> @Lokku, London</li>
-      <li><span className="star">*</span> (Dec 2012) <b>Front-End Engineer</b> @Globant, Buenos Aires</li>
+      <li><span className="star">*</span> (Aug 2016-Feb 2017) <b>Full-Stack Engineer</b> @mycujoo, Amsterdam</li>
+      <li><span className="star">*</span> (Apr 2015-Jul 2016) <b>Front-End Engineer</b> @Osper, London</li>
+      <li><span className="star">*</span> (May 2014-May 2015) <b>Front-End Engineer</b> @Lokku, London</li>
+      <li><span className="star">*</span> (Dec 2012-Mar 2014) <b>Front-End Engineer</b> @Globant, Buenos Aires</li>
     </ul>
   </section>
 );
